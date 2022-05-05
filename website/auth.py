cd1aaaecb7a17f75ba570b354a4f9ee1da1cbaa6
xzan8189 <v.marrazzo12@studenti.unisa.it>
2022-05-05 12:10:29 +0200

    Update files

--- website/auth.py
+++ website/auth.py
@@ -27,7 +27,7 @@ def login():
 
     data = request.form
     print(data)
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -64,4 +64,4 @@ def sign_up():
             flash("Account created!", category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
