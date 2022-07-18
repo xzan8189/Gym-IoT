9061ab9f385b5393fb5f75b99ba84bd04c09c22a
xzan8189 <v.marrazzo12@studenti.unisa.it>
2022-07-18 14:43:05 +0200

    Update files

--- settings/config.py
+++ settings/config.py
@@ -6,9 +6,6 @@ class DefaultConfig:
     # Flask
     SECRET_KEY = os.environ.get("Secret_key", "")
 
-    # IFTTT
-    IFTTT_EVENT_EMAIL_ERROR = os.environ.get("IFTTT_event_email_error", "")
-
     #Telegram bot
     BOT_TOKEN = os.environ.get("Bot_token", "")
     BOT_TOKEN_PAYMENT_PROVIDER = os.environ.get("Bot_token_payment_provider", "")
